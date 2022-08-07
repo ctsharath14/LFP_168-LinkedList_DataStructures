129cc0a1736b547c62a1e9ddf11557b4154dbae7
ctshara14 <ctsharath14@gmail.com>
2022-08-07 22:11:36 +0530

    Adding: Insert 40 after 30 to the Linked List

--- Operations.java
+++ Operations.java
@@ -97,6 +97,26 @@ public class Operations {
         }
     }
 
+    // Method is used to insert after the node in linked list
+    public void insertAfter(int insertAfter, int insertData) {
+        Node temp = head;
+        Node insertNode = new Node(insertData);
+        if (head == null) {
+            System.out.println("Linked List is empty");
+        } else {
+            while (temp != null) {
+                if (temp.data == insertAfter) {
+                    Node node = temp.next;
+                    temp.next = insertNode;
+                    insertNode.next = node;
+                    temp = node;
+                } else {
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+
     // Show method to display the linked list data
     public void PrintList() {
         Node temp = this.head;
